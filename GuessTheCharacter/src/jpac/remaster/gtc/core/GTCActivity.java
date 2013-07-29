523ecb40f68bf020db38e9e612c5f78c1ded56d7
Paulo Carabuena <jkishido@gmail.com>
2013-07-29 20:13:56 +0800

    - Refactor methods - Add comments

--- GuessTheCharacter/src/jpac/remaster/gtc/core/GTCActivity.java
+++ GuessTheCharacter/src/jpac/remaster/gtc/core/GTCActivity.java
@@ -1,3 +1,18 @@
+/******************************************************************************
+ * Copyright 2013
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package jpac.remaster.gtc.core;
 
 import jpac.remaster.gtc.GTCSplash;
@@ -9,6 +24,16 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+/******************************************************************************
+ * Superclass for all activity in this application. All activities must extend
+ * this class to ensure fullscreen feature all throughout.
+ * 
+ * In debug mode, this class will log in four different stages of an activity
+ * lifecycle: onCreate, onPause, onDestroy and onStop.
+ * 
+ * @author JP Carabuena
+ * @since 1.0
+ *****************************************************************************/
 public abstract class GTCActivity extends Activity {
 
 	@Override
@@ -51,25 +76,50 @@ public abstract class GTCActivity extends Activity {
 	@Override
 	protected void onStop() {
 		super.onStop();
-		Util.log(this.getClass().getName() + "[onStop]");
+		Util.log(getClassName() + "[onStop]");
 	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		int flags = getIntent().getFlags();
-		if ((flags & Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY) != 0) {
+		if (isLaunchedFromHistory()) {
 			// The activity was launched from history
 			startActivity(new Intent(this, GTCSplash.class));
 			this.finish();
 		}
 
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setFullscreenWindow();
 
 		ResourceUtil.setContextRef(this);
 		Util.log(this.getClass().getName() + "[onCreate]");
 	}
+
+	/**************************************************************************
+	 * Checks if this activity is launched from history, probably when user
+	 * opens the game from Recent Application.
+	 *************************************************************************/
+	private boolean isLaunchedFromHistory() {
+		int flags = getIntent().getFlags();
+		return (flags & Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY) != 0;
+	}
+
+	/**************************************************************************
+	 * Sets the display window to fullscreen. This will remove the title bar at
+	 * the top of the application as well as the notification bar for lower
+	 * Android SDK level.
+	 *************************************************************************/
+	private void setFullscreenWindow() {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	}
+
+	/**************************************************************************
+	 * Returns the name of this activity.
+	 *************************************************************************/
+	private String getClassName() {
+		return this.getClass().getName();
+	}
+
 }
