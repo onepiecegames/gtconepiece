c1ab7314254d07728b1fadf6a6633259711db605
Juan Paulo Carabuena <carabuena.paulo@klab.com>
2013-08-02 11:48:27 +0800

    remove loading image and label

--- GuessTheCharacter/src/jpac/remaster/gtc/GTCSplash.java
+++ GuessTheCharacter/src/jpac/remaster/gtc/GTCSplash.java
@@ -3,6 +3,7 @@ package jpac.remaster.gtc;
 import jpac.remaster.gtc.core.GTCActivity;
 import jpac.remaster.gtc.data.DataManager;
 import jpac.remaster.gtc.logic.PuzzleManager;
+import jpac.remaster.gtc.util.Constants;
 import jpac.remaster.gtc.util.ResourceLoader;
 import jpac.remaster.gtc.util.ResourceManager;
 import jpac.remaster.gtc.util.SysInfo;
@@ -11,7 +12,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.view.View;
 
 public class GTCSplash extends GTCActivity implements ResourceLoader {
 
@@ -26,8 +26,6 @@ public class GTCSplash extends GTCActivity implements ResourceLoader {
 		setContentView(R.layout.splash);
 
 		SysInfo.loadScreenInfo(this);
-
-		setTypeface(R.id.loadingLabel, ResourceManager.getFont("roboto_thin.ttf"));
 
 		LoadResourceHandler handler = new LoadResourceHandler(this);
 		handler.sendEmptyMessageDelayed(0, Constants.SPLASH_DELAY);
@@ -49,8 +47,6 @@ public class GTCSplash extends GTCActivity implements ResourceLoader {
 
 	@Override
 	public void doLoading() {
-		setVisibility(R.id.progressBar, View.VISIBLE);
-		setVisibility(R.id.loadingLabel, View.VISIBLE);
 		new ResourcesLoaderTask().execute();
 	}
 
@@ -58,7 +54,6 @@ public class GTCSplash extends GTCActivity implements ResourceLoader {
 
 		@Override
 		protected Void doInBackground(Void... params) {
-			getProgressBar(R.id.progressBar).incrementProgressBy(100);
 			ResourceManager.loadSystemFonts();
 			PuzzleManager.init(getApplicationContext());
 			DataManager.init();
