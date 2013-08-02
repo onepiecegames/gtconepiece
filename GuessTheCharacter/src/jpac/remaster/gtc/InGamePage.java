d1cead96d6cfb265af7b50d5e3496624ef6d0e9b
Juan Paulo Carabuena <carabuena.paulo@klab.com>
2013-08-02 12:59:55 +0800

    Added comments to InGamePage

--- GuessTheCharacter/src/jpac/remaster/gtc/InGamePage.java
+++ GuessTheCharacter/src/jpac/remaster/gtc/InGamePage.java
@@ -1,3 +1,18 @@
+/******************************************************************************
+ * Copyright 2013
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package jpac.remaster.gtc;
 
 import jpac.remaster.gtc.core.GTCActivity;
@@ -17,8 +32,18 @@ import android.view.View.OnClickListener;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
+/******************************************************************************
+ * This is the main game activity. This is where the puzzle images will be
+ * displayed to the user.
+ * 
+ * @author JP Carabuena
+ * @since 1.0
+ *****************************************************************************/
 public class InGamePage extends GTCActivity implements UserActionListener {
 
+	// ========================================================================
+	// Request codes.
+	// ========================================================================
 	private static final int REQUEST_SHOW_CONFIRM_BACK = 0;
 	private static final int REQUEST_SHOW_ACKNOWLEDGE = 1;
 	private static final int REQUEST_CONFIRM_REVEAL = 2;
@@ -27,8 +52,14 @@ public class InGamePage extends GTCActivity implements UserActionListener {
 	private static final int REQUEST_SHARE_FACEBOOK = 5;
 	private static final int REQUEST_PUBLISH_FEED = 6;
 
+	// ========================================================================
+	// The current puzzle.
+	// ========================================================================
 	private Puzzle puzzle;
 
+	// ========================================================================
+	// This is the button manager for the current level.
+	// ========================================================================
 	private ButtonManager buttonManager;
 
 	@Override
@@ -46,6 +77,7 @@ public class InGamePage extends GTCActivity implements UserActionListener {
 
 		buttonManager = ButtonManager.getInstance();
 
+		// loads the puzzle
 		if (PuzzleManager.isFinished()) {
 			puzzle = null;
 		} else if (DataManager.checkCurrentPuzzleId() == -1) {
@@ -69,6 +101,7 @@ public class InGamePage extends GTCActivity implements UserActionListener {
 			String removedButtonMeta = DataManager.getRemovedButtons();
 			String lockStateMeta = DataManager.getLockedButtons();
 
+			// initialize choice buttons and answer layout
 			buttonManager.init(this, puzzle);
 			buttonManager.setChoices(PuzzleManager.createChoiceSet(
 					puzzle.getAnswer(), puzzle.getRandomSeed()));
@@ -92,6 +125,9 @@ public class InGamePage extends GTCActivity implements UserActionListener {
 		}
 	}
 
+	/**************************************************************************
+	 * Update the current level from the shared preferences.
+	 *************************************************************************/
 	private void updateLevel() {
 		setText(R.id.currLevelLabel, "" + DataManager.checkLevel());
 		setTypeface(R.id.currLevelLabel,
@@ -100,6 +136,9 @@ public class InGamePage extends GTCActivity implements UserActionListener {
 				ResourceManager.getFont("digitalstrip.ttf"));
 	}
 
+	/**************************************************************************
+	 * Update the current gold from the shared preferences.
+	 *************************************************************************/
 	private void updateGold() {
 		setText(R.id.amountLabel, "" + DataManager.checkGold());
 		setTypeface(R.id.amountLabel,
@@ -107,6 +146,9 @@ public class InGamePage extends GTCActivity implements UserActionListener {
 		setTypeface(R.id.goldLabel, ResourceManager.getFont("digitalstrip.ttf"));
 	}
 
+	/**************************************************************************
+	 * Update the current puzzle difficulty from the shared preferences.
+	 *************************************************************************/
 	private void updatePuzzleDifficulty() {
 		int level = puzzle.getDifficulty();
 		int drawable = R.drawable.level_full;
@@ -125,6 +167,9 @@ public class InGamePage extends GTCActivity implements UserActionListener {
 		}
 	}
 
+	/**************************************************************************
+	 * Use the remove letter hint.
+	 *************************************************************************/
 	public void removeLetter(View v) {
 		if (DataManager.isGoldEnough(Constants.REMOVE_COST)) {
 			showConfirmUseHint("Pay " + Constants.REMOVE_COST
@@ -135,6 +180,9 @@ public class InGamePage extends GTCActivity implements UserActionListener {
 		}
 	}
 
+	/**************************************************************************
+	 * Use the reveal letter hint.
+	 *************************************************************************/
 	public void revealLetter(View v) {
 		if (DataManager.isGoldEnough(Constants.REVEAL_COST)) {
 			showConfirmUseHint("Pay " + Constants.REVEAL_COST
@@ -145,6 +193,9 @@ public class InGamePage extends GTCActivity implements UserActionListener {
 		}
 	}
 
+	/**************************************************************************
+	 * Solve the puzzle and advance to the next level.
+	 *************************************************************************/
 	public void solvePuzzle(View v) {
 		if (DataManager.isGoldEnough(Constants.SOLVE_COST)) {
 			showConfirmUseHint("Pay " + Constants.SOLVE_COST
@@ -154,6 +205,9 @@ public class InGamePage extends GTCActivity implements UserActionListener {
 		}
 	}
 
+	/**************************************************************************
+	 * Ask facebook friends for help.
+	 *************************************************************************/
 	public void askFacebook(View v) {
 		if (DataManager.checkIfPosted(puzzle.getId())) {
 			startActivityForResult(
@@ -173,12 +227,18 @@ public class InGamePage extends GTCActivity implements UserActionListener {
 		}
 	}
 
+	/**************************************************************************
+	 * Display confirmation popup for using hints.
+	 *************************************************************************/
 	private void showConfirmUseHint(String message, int requestCode) {
 		startActivityForResult(
 				Util.createConfirmPopup(this, "Confirm Use Hint", message),
 				requestCode);
 	}
 
+	/**************************************************************************
+	 * Display notification that user have insufficient gold.
+	 *************************************************************************/
 	private void showInsufficientGold(int cost) {
 		startActivityForResult(Util.createAcknowledgePopup(this,
 				"Insufficient Gold", "You need at least " + cost
@@ -247,6 +307,9 @@ public class InGamePage extends GTCActivity implements UserActionListener {
 		}
 	}
 
+	/**************************************************************************
+	 * Display the level completed page.
+	 *************************************************************************/
 	private void showLevelComplete() {
 		PuzzleManager.markAsSolved(puzzle.getId());
 		int prize = puzzle.getDifficulty() * Constants.PUZZLE_PRIZE;
